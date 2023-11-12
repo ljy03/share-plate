a595a460489e7a4c3d51c412215e67cdbe9425ef
KlausMikhaelson <satyamsingh5076@gmail.com>
2023-11-12 02:40:09 -0600

    fixed merge issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,19 @@ import React, { createContext, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './Home';
-<<<<<<< HEAD
-import { useContext } from 'react';
-import Navbar from './Navbar';
-=======
 import List from './pages/List';
->>>>>>> toby
+import Navbar from './Navbar';
 
 export const MyContextProvider = createContext();
 
 export const backendUrl = "http://localhost:3001/api"
 
 function App() {
-<<<<<<< HEAD
   const [user, setUser] = useState(null);
 
   return (
     <>
       <MyContextProvider.Provider value={{user, setUser}}>
-=======
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <MyContextProvider.Provider value={{ count, setCount }}>
->>>>>>> toby
         <BrowserRouter>
         <Navbar />
           <Routes>
